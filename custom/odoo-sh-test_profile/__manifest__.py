9c9a4d77a74d973973d10bd7197079bdf1436c1e
Vincent COFFIN <vco@subteno.com>
2021-06-11 14:40:09 +0200

    [ADD] Add base of the customer profile module

---
+++ custom/odoo-sh-test_profile/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 SYLEAM
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'odoo-sh-test Profile',
+    'version': '14.0.1.0.0',
+    'category': 'Custom',
+    'description': """Profile for odoo-sh-test""",
+    'author': 'Subteno',
+    'website': 'https://www.subteno-it.fr/',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'data/base.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'AGPL-3',
+}
